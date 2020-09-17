3cd71c50ca9ee852cbbbd541625295639f4abfc5
Hannah Hazi <hannah@anvil.works>
2020-09-17 11:49:48 +0100

    Standardise on docker-compose where possible - can't do it everywhere because of lack of nvidia gpu support in docker-compose.

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
-docker build -t hhazi/tatooine:3.5 .
-docker run -td --name tatooine -i -p 8000:8888 --rm -v $(pwd):/star_wars_hackathon hhazi/tatooine:3.5
+docker-compose up -d
