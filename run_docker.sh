57dcb3acaec4d6da55bcd11ca0ea1e34620a80c0
Hannah Hazi <hannah@anvil.works>
2020-09-24 14:29:22 +0100

    Get rid of docker-compose for now.

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
-docker-compose up -d
+docker build -t hhazi/tatooine:3.5 .
+docker run -td --name tatooine -i -p 8000:8888 --rm -v $(pwd):/star_wars_hackathon hhazi/tatooine:3.5
--- run_docker_gpu.sh
+++ run_docker_gpu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
-# We can't use docker-compose for running this with GPU resources (--gpus all)
-# This is because of the limitations from https://github.com/docker/compose/issues/6691
 docker build -t hhazi/tatooine:3.5 .
 docker run -td --name tatooine -i -p 8000:8888 --gpus all --rm -v $(pwd):/star_wars_hackathon hhazi/tatooine:3.5
