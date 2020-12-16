c96f079c99828b3cd61890b35944ce5e4c7b5730
Hannah Hazi <hannah.hazi@cantab.net>
2020-12-16 13:04:19 +0000

    Update run_docker.sh
    Keep container alive and try to revive it if the server goes down.

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,4 @@
 # -*- ENCODING: UTF-8 -*-
 
 docker build -t hhazi/tatooine:3.5 .
-docker run -td --name tatooine -i -p 8000:8888 --rm -v $(pwd):/star_wars_hackathon hhazi/tatooine:3.5
+docker run -td --name tatooine -i -p 8000:8888 --restart unless-stopped -v $(pwd):/star_wars_hackathon hhazi/tatooine:3.5
